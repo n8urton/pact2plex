52110abcb924038f5b4af8f87d3d7e5df8eb3c9e
nic <13386153+n8urton@users.noreply.github.com>
2019-01-08 17:33:48 -0800

    working test data-struct

---
+++ data_test.py
@@ -0,0 +1,14 @@
+import json
+
+show_title = "Mastering Linux Security and Hardening"
+
+with open('test_data.json') as infile:
+    data = json.load(infile)
+# print(data)
+print(show_title)
+for season_num, season in enumerate(data, start=1):
+    title = season["title"]
+    print(f"\tSeason {season_num} - {title}")
+    for episode_num, episode in (enumerate(season["episode"], start=1)):
+        old_name, new_name = episode["old_name"], episode["new_name"]
+        print(f"\t\t{old_name} . . . {show_title} - s{season_num:02}e{episode_num:02} - {new_name}")
